2d98bbc6a2477667ba6b19cb95121bc9dcf79585
Aleksandr Belash <belash.aleksandr777@gmail.com>
2024-10-21 05:12:42 +0300

    Tob bar addition

--- Application/app/src/main/java/com/nalek0/navigation/MainActivity.kt
+++ Application/app/src/main/java/com/nalek0/navigation/MainActivity.kt
@@ -3,27 +3,41 @@ package com.nalek0.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nalek0.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             NavigationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { TopBar() },
+                ) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        Content()
+                    }
                 }
             }
         }
@@ -31,17 +45,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopBar() {
+    NavigationTheme {
+        Row(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.tertiary)
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = ::upButtonPressed,
+                modifier = Modifier.wrapContentSize(),
+                shape = RoundedCornerShape(5.dp),
+            ) {
+                Text("<<<")
+            }
+            Text(text = stringResource(id = R.string.app_name), fontSize = 18.sp)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+fun upButtonPressed() {
+    // TDOO
+}
+
 @Composable
-fun GreetingPreview() {
-    NavigationTheme {
-        Greeting("Android")
-    }
+fun Content() {
+    Text("Hello world")
 }
